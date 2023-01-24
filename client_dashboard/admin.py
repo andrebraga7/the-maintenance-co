eae109bc0213f8103ce062da4bde173e3633760c
andrebraga7 <andrestrevisan@gmail.com>
2023-01-24 20:16:37 +0000

    Update the job title in the Job model

--- client_dashboard/admin.py
+++ client_dashboard/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Equipment, Job
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Equipment)
+admin.site.register(Job)
--- client_dashboard/models.py
+++ client_dashboard/models.py
@@ -2,18 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# CATEGORIES = (
-#     (0, 'Kitchen'),
-#     (1, 'Front Counter'),
-#     (2, 'Dinning Area'),
-#     (3, 'Back Area'),
-#     (4, 'Building Facilities'),
-#     (5, 'Eletrical'),
-#     (6, 'Other'))
-
-
 class Category(models.Model):
     name = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -31,22 +22,22 @@ class Job(models.Model):
     title = models.CharField(max_length=50, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
-    # client = models.ForeignKey(User, on_delete=models.CASCADE)
+    client = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField(max_length=200)
     assignment = models.CharField(max_length=20, default='unassigned')
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(default=0)
-    feedback = models.TextField(max_length=200)
+    feedback = models.TextField(max_length=200, blank=True)
     deletion = models.BooleanField(default=False)
 
     def generate_title(self):
-        return f'Job #{self.id}, {self.equipment} in {self.category}'
+        return f'Job for {self.equipment} in {self.category}'
 
     def save(self, *args, **kwargs):
         if not self.title:
             self.title = self.generate_title()
-        super(Subject, self).save(*args, **kwargs)
+        super(Job, self).save(*args, **kwargs)
 
     class Meta:
         permissions = [
