05453ba22586cb8cf1cd7db6f5bbf53d571bf470
andrebraga7 <andrestrevisan@gmail.com>
2023-01-24 18:59:45 +0000

    Create category and equipment models

--- client_dashboard/models.py
+++ client_dashboard/models.py
@@ -1,19 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-CATEGORIES = (
-    (0, 'Kitchen'),
-    (1, 'Front Counter'),
-    (2, 'Dinning Area'),
-    (3, 'Back Area'),
-    (4, 'Building Facilities'),
-    (5, 'Eletrical'),
-    (6, 'Other'))
+# CATEGORIES = (
+#     (0, 'Kitchen'),
+#     (1, 'Front Counter'),
+#     (2, 'Dinning Area'),
+#     (3, 'Back Area'),
+#     (4, 'Building Facilities'),
+#     (5, 'Eletrical'),
+#     (6, 'Other'))
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Equipment(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 
 class Job(models.Model):
-    category = models.CharField(choices=CATEGORIES,max_length=50, default=0)
-    # equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
     # client = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField(max_length=200)
     assignment = models.CharField(max_length=20, default='unassigned')
@@ -23,5 +40,13 @@ class Job(models.Model):
     feedback = models.TextField(max_length=200)
     deletion = models.BooleanField(default=False)
 
+    def generate_title(self):
+        return f'Job #{self.id}, {self.equipment} in {self.category}'
 
-# class Equipment(models.Model):
+    def save(self, *args, **kwargs):
+        if not self.title:
+            self.title = self.generate_title()
+        super(Subject, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
