0b7b60e07891bab890c0977426e475d66b3a269e
andrebraga7 <andrestrevisan@gmail.com>
2023-01-24 18:33:06 +0000

    Create the Job model

--- client_dashboard/models.py
+++ client_dashboard/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+CATEGORIES = (
+    (0, 'Kitchen'),
+    (1, 'Front Counter'),
+    (2, 'Dinning Area'),
+    (3, 'Back Area'),
+    (4, 'Building Facilities'),
+    (5, 'Eletrical'),
+    (6, 'Other'))
+
+
+class Job(models.Model):
+    category = models.CharField(choices=CATEGORIES,max_length=50, default=0)
+    # equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
+    # client = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField(max_length=200)
+    assignment = models.CharField(max_length=20, default='unassigned')
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(default=0)
+    feedback = models.TextField(max_length=200)
+    deletion = models.BooleanField(default=False)
+
+
+# class Equipment(models.Model):
