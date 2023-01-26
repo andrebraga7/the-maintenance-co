d66bf7657a4efbaf9ef533a82ed95bb8056270c6
andrebraga7 <andrestrevisan@gmail.com>
2023-01-26 01:33:13 +0000

    Add the dashboard class to the views.py to redirect the manager user to the manager dashboard

--- home_page/urls.py
+++ home_page/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
+    path('dashboard/', views.Dashboard.as_view(), name='dashboard')
 ]
--- home_page/views.py
+++ home_page/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth.models import Permission
 
 
 class Home(View):
 
     def get(self, request):
         return render(request, 'home_page/home.html')
+
+
+class Dashboard(View):
+
+    def get(self, request):
+
+        if request.user.has_perm('client_dashboard.is_manager'):
+            return redirect('manager')
+        else:
+            return redirect('home')
