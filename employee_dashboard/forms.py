0615afa854f1ab1ac1120b4c7f8212949dd8e375
andrebraga7 <andrestrevisan@gmail.com>
2023-02-15 23:20:21 +0000

    Restyle the employee active jobs template

--- employee_dashboard/forms.py
+++ employee_dashboard/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout
+from crispy_bootstrap5.bootstrap5 import FloatingField
 from client_dashboard.models import Job
 
 
@@ -6,3 +9,11 @@ class FeedbackForm(forms.ModelForm):
     class Meta:
         model = Job
         fields = ['feedback']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.layout = Layout(
+            FloatingField('feedback')
+        )
