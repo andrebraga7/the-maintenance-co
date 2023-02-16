a2920f88bd572943f68ac148bd6ecf178ed86bec
andrebraga7 <andrestrevisan@gmail.com>
2023-02-16 21:29:31 +0000

    Connect the contact form to emailJS

--- static/js/email.js
+++ static/js/email.js
@@ -1,13 +1,29 @@
-service_3a4d0k4
+$(document).ready(function() {
 
-var templateParams = {
-    name: 'James',
-    notes: 'Check this out!'
-};
- 
-emailjs.send('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', templateParams)
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
-    });
+    // // Capture the contact form
+    $('#contact-form').submit(function(event) {
+        event.preventDefault();
+        let form_message = $("#id_message").val();
+        let form_subject = $("#id_subject").val();
+        let form_email = $("#id_email").val();
+
+
+        var templateParams = {
+            message: form_message,
+            subject: form_subject,
+            email: form_email,
+        };
+    
+        emailjs.send("service_3a4d0k4", "the_maintenance_co", templateParams)
+            .then(function(response) {
+            console.log('SUCCESS!', response.status, response.text);
+            }, function(error) {
+            console.log('FAILED...', error);
+            });
+        
+        // Resets the contact form
+        this.reset();
+
+    })
+
+})
