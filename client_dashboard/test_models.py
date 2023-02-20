2bfede526d0aff0552a4b82f7ec9af4dce93b4cf
andrebraga7 <andrestrevisan@gmail.com>
2023-02-20 04:33:30 +0000

    Create tests for the client_dashboard models

---
+++ client_dashboard/test_models.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Category, Equipment
+from manager_dashboard.models import Profile
+
+
+class TestModel(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.test_user = User.objects.create(
+            username='testuser',
+            email='email@email.com',
+            profile=Profile(name='Test Name')
+            )
+
+        cls.category = Category.objects.create(
+            name='Test Category', user=cls.test_user)
+
+        cls.equipment = Equipment.objects.create(
+            name='Test Equipment', category=cls.category, user=cls.test_user)
+
+    def test_type_default_to_3(self):
+        self.assertEqual(self.test_user.profile.type, 3)
+
+    def test_category_has_foreign_user(self):
+        self.assertEqual(self.category.user, self.test_user)
+
+    def test_equipment_has_correct_name(self):
+        self.assertEqual(self.equipment.name, 'Test Equipment')
